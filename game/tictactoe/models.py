9676d4c10ed9c30cc4919bd510f332810ee4353c
Moran-Griffin <moran2gc@dukes.jmu.edu>
2026-02-15 15:20:11 -0500

    created game user models

--- game/tictactoe/models.py
+++ game/tictactoe/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+
+class Game(models.Model):
+    """Model representing the game"""
+    id = models.IntegerField
+    player_x_id = models.ManyToManyField("User")
+    player_y_id = models.ManyToManyField("User")
+    board = models.JSONField
+    turn = models.BooleanField  # True for X, False for O
+    game_active = models.BooleanField
+    mini_board_winners = models.JSONField
+    current_board = models.IntegerField
+    next_board = models.IntegerField
+
+
+class User(models.Model):
+    id = models.IntegerField
